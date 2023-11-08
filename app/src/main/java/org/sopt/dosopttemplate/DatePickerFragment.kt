0bd5ca5f7d8c9c68334c1cc26b7b891d3c00e7d9
pump9918 <hoyeon9918@gmail.com>
2023-11-08 09:47:08 +0900

    [feat] : DatePicker 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/DatePickerFragment.kt
@@ -0,0 +1,27 @@
+package org.sopt.dosopttemplate
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.EditText
+import androidx.fragment.app.DialogFragment
+import java.util.Calendar
+import java.util.Locale
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(requireActivity(), this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val selectedDate = String.format(Locale.getDefault(), "%02d-%02d-%02d", year, month + 1, dayOfMonth)
+        val editText = activity?.findViewById<EditText>(R.id.deadlineEdit)
+        editText?.setText(selectedDate)
+    }
+}
--- app/src/main/java/org/sopt/dosopttemplate/DoAndroidFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/DoAndroidFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import org.sopt.dosopttemplate.databinding.FragmentDoAndroidBinding
 
@@ -25,6 +27,22 @@ class DoAndroidFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val dateEditText = view.findViewById<EditText>(R.id.deadlineEdit)
+        val datePickerButton = view.findViewById<Button>(R.id.datePickerButton)
+
+        dateEditText.setOnClickListener {
+            showDatePickerDialog(view)
+        }
+
+        datePickerButton.setOnClickListener {
+            showDatePickerDialog(view)
+        }
+    }
+
+    fun showDatePickerDialog(view: View) {
+        val newFragment = DatePickerFragment()
+        newFragment.show(childFragmentManager, "datePicker")
     }
 
     override fun onDestroyView() {
